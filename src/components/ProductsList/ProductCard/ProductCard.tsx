70af3812bb9801c7ead4c9167fd4276ffa1b4bed
rubhain <bugrahantufekcioglu@gmail.com>
2023-01-09 15:13:05 +0300

    Buy - Sell btn

--- src/components/ProductsList/ProductCard/ProductCard.tsx
+++ src/components/ProductsList/ProductCard/ProductCard.tsx
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Button, Card } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { budgetCounter } from "../../../storage/products/productsSlice";
 
 function ProductCard(props: any) {
+  const [count, setCount] = useState(0);
+
   const product = props.item;
+  const budget = useSelector((state: any) => state.products.budget);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(budgetCounter({ id: product.id, count: count }));
+  }, [count]);
+
+  const buyItem = () => {
+    setCount(Number(count) + 1);
+  };
+
+  const sellItem = () => {
+    setCount(Number(count) - 1);
+  };
 
   return (
     <div>
@@ -12,8 +30,13 @@ function ProductCard(props: any) {
         <Card.Body>
           <Card.Title>{product.productName}</Card.Title>
           <Card.Text>${product.productPrice}</Card.Text>
-          <Button variant="secondary">Sell</Button>
-          <Button variant="primary">Buy</Button>
+          <Button variant="secondary" onClick={sellItem}>
+            Sell
+          </Button>
+          <input type="number" value={count}></input>
+          <Button variant="primary" onClick={buyItem}>
+            Buy
+          </Button>
         </Card.Body>
       </Card>
     </div>
--- src/storage/products/productsSlice.ts
+++ src/storage/products/productsSlice.ts
@@ -8,9 +8,23 @@ export const productsSlice = createSlice({
     initialState: {
             items: products,
             budget: 100000000000,
+            initialMoney: 100000000000,
     },
     reducers:{
-        budgetCounter:(state, action)=>{}
+        budgetCounter:(state, action)=>{
+            //setting count data
+            const { count } = action.payload;
+            const product = state.items.find(
+              (product) => product.id === action.payload.id
+            );
+            product!.count = count;
+            //budget update
+            let price = 0;
+            state.items.map((product) => {
+              price += Number(product.count) * Number(product.productPrice);
+            });
+            state.budget = Number(state.initialMoney) - Number(price);
+        }
 
     }
 });
